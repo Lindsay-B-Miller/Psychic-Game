73fdaaa564ae53fd877157962cac2ea7d26779ae
Lindsay <lindsay.lbm@gmail.com>
2019-09-19 08:36:01 -0500

    cleaned code and fixed resetting at 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,13 +19,13 @@ var reset = function () {
 
 
 
-//function for guesses=0
-var noMoreGuess = function () {
-    losses++;
-    guessesLeft = 9;
-    alreadyTried = [];
-    computeerLetter = letters[Math.floor(Math.random() * letters.length)];
-}
+// //function for guesses=0
+// var noMoreGuess = function () {
+//     losses++;
+//     guessesLeft = 9;
+//     alreadyTried = [];
+//     computeerLetter = letters[Math.floor(Math.random() * letters.length)];
+// }
 
 // //function for win
 // function winner() {
@@ -38,43 +38,50 @@ document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     console.log(userGuess)
 
-    //function for the number of guesses remaining and letters guessed already
-    function numberGuessesRemain() {
+
+    if (letters.isIndexOf(userGuess) > -1 {
+
+        //function for the number of guesses remaining and letters guessed already
+        function numberGuessesRemain() {
+            document.querySelector("#remaining-guesses").innerHTML = "Guesses Left: " + guessesLeft;
+        }
+
+        function guessesAlready() {
+            document.querySelector("#already-tried").innerHTML = "Your guesses so far: " + alreadyTried + " ";
+        }
+
+
+
+        //comapre user and computer guess
+        if (computerLetter === userGuess) {
+            wins++;
+            reset();
+        }
+
+        else if (alreadyTried.includes(userGuess)) {
+            alert("You have already selected this letter. Please try a different letter")
+        }
+
+        else if (guessesLeft > 0) {
+            guessesLeft--;
+            alreadyTried.push(userGuess);
+            numberGuessesRemain();
+            guessesAlready();
+
+        }
+
+        if (guessesLeft === 0) {
+            losses++;
+            reset();
+        }
+
+
+
+
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
         document.querySelector("#remaining-guesses").innerHTML = "Guesses Left: " + guessesLeft;
-    }
-
-    function guessesAlready() {
-        document.querySelector("#already-tried").innerHTML = "Your guesses so far: " + alreadyTried + " ";
-    }
-
-
-    //comapre user and computer guess
-    if (computerLetter === userGuess) {
-        wins++;
-        reset();
-    }
-
-    else if (alreadyTried.includes(userGuess)) {
-        alert("You have already selected this letter. Please try a different letter")
-    }
-
-    else if (guessesLeft > 0) {
-        guessesLeft--;
-        alreadyTried.push(userGuess);
-        numberGuessesRemain();
-        guessesAlready();
+        document.querySelector("#already-tried").innerHTML = "Your guesses so far: " + alreadyTried;
 
     }
-    else if (guessesLeft === 0) {
-        noMoreGuess()
-    }
-
-
-
-
-    document.querySelector('#losses').innerHTML = "Losses: " + losses;
-    document.querySelector('#wins').innerHTML = "Wins: " + wins;
-    document.querySelector("#remaining-guesses").innerHTML = "Guesses Left: " + guessesLeft;
-    document.querySelector("#already-tried").innerHTML = "Your guesses so far: " + alreadyTried;
-
 }
